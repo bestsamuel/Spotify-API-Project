ae2a1a290b93414b2931eabfe7cbbec76a560ce8
Samuel Best <140278798+bestsamuel@users.noreply.github.com>
2023-10-17 21:23:08 -0400

    Update app.py

--- SpotifyProject/app.py
+++ SpotifyProject/app.py
@@ -7,7 +7,7 @@ import base64
 from requests import post, get
 import json
 
-# Load environment variables
+
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
